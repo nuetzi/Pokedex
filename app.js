2474e169567c64e08bd95d2ccb2bd82fe5a99841
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-02-28 11:48:13 -0500

    More formatting updates

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const fetchData = () => {
     })
     .then((data) => {
         let adjustedName = data.name.split("-").join(" ");          // Replace hyphens with spaces
-        pokemonName.innerHTML = `${adjustedName}`;
-        pokemonNumber.innerHTML = `Pokédex #: ${data.id}`;
+        pokemonName.innerHTML = `<u>${adjustedName}</u>`;
+        pokemonNumber.innerHTML = `Pokédex ID#: ${data.id}`;
     
         if (data.types.length > 1) {
             pokemonType.innerHTML = `Type: ${data.types[0].type.name} / ${data.types[1].type.name}`;        // For dual-type Pokemon
@@ -28,43 +28,38 @@ const fetchData = () => {
             feet += 1;                                      // So this corrects by adjusting up to the next foot
             inchesRemainder = 0;                            // With 0 inches leftover
         }
-        pokemonHeight.innerHTML = `Height: ${feet}'${inchesRemainder}"  / ${meters} m`;
     
         let kgs = (data.weight / 10).toFixed(1);                                // Converts weight info to kg with 1 decimal place
         let lbs = (data.weight * 0.2204623).toFixed(1);                         // Converts weight info to lbs with 1 decimal place
-        pokemonWeight.innerHTML = `Weight: ${lbs} lbs  /  ${kgs} kg`;
+
+        pokemonMeasurements.innerHTML = `Height: ${feet}'${inchesRemainder}"  / ${meters} m <br> Weight: ${lbs} lbs  /  ${kgs} kg`;
 
         let ability1 = data.abilities[0].ability.name.split("-").join(" ");             // Replaces hyphens with space
-        pokemonAbility1.innerHTML = `Ability 1: ${ability1}`;
+        
         if (data.abilities.length > 1) {                                                // Check for multiple abilities
             let ability2 = data.abilities[1].ability.name.split("-").join(" ");
-            pokemonAbility2.innerHTML = `Ability 2: ${ability2}`;
             if (data.abilities.length > 2) {
                 let ability3 = data.abilities[2].ability.name.split("-").join(" ");     // No Pokémon has more than 3 abilities
-                pokemonAbility3.innerHTML = `Ability 3: ${ability3}`;
+                pokemonAbilities.innerHTML = `Abilities: <br> ${ability1} <br> ${ability2} <br> ${ability3}`;
             }
             else {
-                pokemonAbility3.innerHTML = "";             // Prevents carrying over abilities from a previous lookup
+                pokemonAbilities.innerHTML = `Abilities: <br> ${ability1} <br> ${ability2}`;
             }
         }
         else {
-            pokemonAbility2.innerHTML = "";
-            pokemonAbility3.innerHTML = "";
+            pokemonAbilities.innerHTML = `Ability: <br> ${ability1}`;
         }
 
         pokemonArtwork.innerHTML = `<img src=${data.sprites.other["official-artwork"].front_default} width="250">`;
-        pokemonSpriteDefault.innerHTML = `Normal: <br> <img src=${data.sprites.front_default}>`;
-        pokemonSpriteShiny.innerHTML = `Shiny: <br> <img src=${data.sprites.front_shiny}>`;
+        pokemonSpriteDefault.innerHTML = `<u>Normal:</u> <br> <img src=${data.sprites.front_default}>`;
+        pokemonSpriteShiny.innerHTML = `<u>Shiny:</u> <br> <img src=${data.sprites.front_shiny}>`;
     })
     .catch(() => {
-        pokemonName.innerHTML = "Error: Invalid Pokémon Number";        // Clears all fields and displays an error message for invalid lookups
-        pokemonNumber.innerHTML = "";
-        pokemonType.innerHTML = "";
-        pokemonHeight.innerHTML = "";
-        pokemonWeight.innerHTML = "";
-        pokemonAbility1.innerHTML = "";
-        pokemonAbility2.innerHTML = "";
-        pokemonAbility3.innerHTML = "";
+        pokemonName.innerHTML = "Error: Unknown Pokémon";        // Clears all fields and displays an error message for invalid lookups
+        pokemonNumber.innerHTML = "Pokédex ID#: MissingNo.";
+        pokemonType.innerHTML = "Type: -----";
+        pokemonMeasurements.innerHTML = "Height: ----- <br> Weight: -----";
+        pokemonAbilities.innerHTML = "Abilities: -----";
         pokemonArtwork.innerHTML = `<img src="./missingno.png" width="250">`;
         pokemonSpriteDefault.innerHTML = "";
         pokemonSpriteShiny.innerHTML = "";
